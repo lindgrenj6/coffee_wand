1c8b352e70c76b8f0ed4ceab97b4aa44b91f05ff
Jacob Lindgren <jlindgre@redhat.com>
2020-05-02 16:59:28 -0500

    covfefe logic complete

--- lib/covfefe.ex
+++ lib/covfefe.ex
@@ -1,18 +1,27 @@
 defmodule Covfefe do
+  @covfefe_file "/home/jlindgren/.covfefe"
+
   def main([]), do: covfefe()
-  def main(["on"]), do: covfefe(:ON)
-  def main(["off"]), do: covfefe(:OFF)
+  def main(["on"]), do: run_covfefe(:ON)
+  def main(["off"]), do: run_covfefe(:OFF)
 
   defp covfefe do
-    stat = File.lstat!("/home/jlindgren/.covfefe", time: :local)
+    {{_, _, day}, _} = File.lstat!(@covfefe_file, time: :local).mtime
+
+    if day != DateTime.utc_now().day do
+      run_covfefe(:ON)
+      :timer.sleep(2 * 1000)
+      File.touch(@covfefe_file)
+      System.cmd("espeak-ng", ["covfehfee brewing"])
+      :timer.sleep(12 * 60 * 1000)
+      System.cmd("espeak-ng", ["covfehfee ready"])
+    end
   end
 
-  defp covfefe(cmd) do
+  defp run_covfefe(cmd) when is_atom(cmd) do
     case System.cmd("docker", ~w(run --rm #{image()} -h #{host()} --action #{cmd})) do
-      {_output, 0} ->
-        IO.puts "Covfefe is #{cmd}"
-      {_output, code} ->
-        IO.puts "Something happened, rc: #{code}"
+      {_output, 0} -> IO.puts("Covfefe is #{cmd}")
+      {_output, code} -> IO.puts("Something happened, rc: #{code}")
     end
   end
 
--- mix.exs
+++ mix.exs
@@ -24,7 +24,7 @@ defmodule Covfefe.MixProject do
     [
       # {:dep_from_hexpm, "~> 0.3.0"},
       # {:dep_from_git, git: "https://github.com/elixir-lang/my_dep.git", tag: "0.1.0"}
-      {:docker_api, "~> 0.1.3"}
+      # {:docker_api, "~> 0.1.3"}
     ]
   end
 
